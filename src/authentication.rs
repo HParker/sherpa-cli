3ea8e250e0455851fc80795a1261e181508780c6
Michael Stock <mikeastock@gmail.com>
2017-02-04 09:27:23 -0800

    Return Response struct

--- src/authentication.rs
+++ src/authentication.rs
@@ -4,16 +4,22 @@ use std::collections::HashMap;
 
 const BASE_URL: &'static str = "https://sherpa.procoretech.com/api/v1";
 
+#[derive(Debug, Deserialize)]
+struct TokenResponse {
+    token: String,
+    expires_at: String,
+}
+
 pub fn run(matches: &ArgMatches) {
     let github_token = matches
         .value_of("token")
         .expect("Expected required token");
 
-    let token = request_token(github_token);
-    println!("{:?}", token);
+    let response = request_token(github_token);
+    println!("{:?}", response);
 }
 
-fn request_token(github_token: &str) -> Result<HashMap<String, String>, ReqwestError> {
+fn request_token(github_token: &str) -> Result<TokenResponse, ReqwestError> {
     let client = try!(Client::new());
     let url = BASE_URL.to_owned() + "/token";
 
@@ -25,5 +31,5 @@ fn request_token(github_token: &str) -> Result<HashMap<String, String>, ReqwestE
 
     let response = client.post(&url).json(&body).send();
 
-    try!(response.map(|mut res| res.json()))
+    try!(response.map(|mut res| res.json::<TokenResponse>()))
 }
